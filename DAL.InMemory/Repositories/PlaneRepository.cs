c74749b09b8401fbf57a007fd2ceb05942bc4053
Gregory Golovko <golovko.greg@gmail.com>
2018-07-15 01:42:12 +0300

    Realized data access layer for im memory testing use

---
+++ DAL.InMemory/Repositories/PlaneRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models;
+
+namespace DAL.InMemory.Repositories
+{
+    internal class PlaneRepository : BaseRepository<Plane>
+    {
+
+        public PlaneRepository(AppContext appContext) : base(appContext)
+        {
+        }
+
+        public override Plane Create(Plane plane)
+        {
+            _appContext.Planes.Add(plane);
+            var pl = _appContext.Planes.Find(p => p.Equals(plane));
+            return pl;
+        }
+
+        public override void Delete(Guid id)
+        {
+            var plane = _appContext.Planes.Find(p => p.Id == id);
+            if (plane != null)
+                _appContext.Planes.Remove(plane);
+        }
+
+        public override Plane Get(Guid id)
+        {
+            return _appContext.Planes.Find(p => p.Id == id);
+        }
+
+        public override IEnumerable<Plane> GetList()
+        {
+            return _appContext.Planes;
+        }
+
+        public override void Update(Plane plane)
+        {
+            var entity = _appContext.Planes.Find(p => p.Id == plane.Id);
+
+            if (entity != null)
+            {
+                entity.Name = plane.Name;
+                entity.Lifetime = plane.Lifetime;
+                entity.ReleaseDate = plane.ReleaseDate;
+                entity.Type = plane.Type;
+            }
+        }
+    }
+}
---
+++ DAL.InMemory/RepositoryUnit.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.InMemory.Repositories;
+using Models;
+
+namespace DAL.InMemory
+{
+    public class RepositoryUnit : IRepositoryUnit
+    {
+        private readonly AppContext _appContext;
+
+        private AircrewRepository _aircrewRepository;
+        private DepartureRepository _departureRepository;
+        private FlightRepository _flightRepository;
+        private PilotRepository _pilotRepository;
+        private PlaneRepository _planeRepository;
+        private PlaneTypeRepository _planeTypeRepository;
+        private StewardRepository _stewardRepository;
+        private TicketRepository _ticketRepository;
+
+        public RepositoryUnit()
+        {
+            _appContext = new AppContext();
+        }
+
+        public IRepository<Departure> DepartureRepository => _departureRepository ??
+                                                             (_departureRepository = new DepartureRepository(_appContext));
+
+        public IRepository<Flight> FlightRepository => _flightRepository ??
+                                                       (_flightRepository = new FlightRepository(_appContext));
+
+        public IRepository<Aircrew> AircrewRepository => _aircrewRepository ?? 
+                                                         (_aircrewRepository = new AircrewRepository(_appContext));
+
+        public IRepository<Pilot> PilotRepository => _pilotRepository ??
+                                                     (_pilotRepository = new PilotRepository(_appContext));
+
+        public IRepository<Plane> PlaneRepository => _planeRepository ??
+                                                     (_planeRepository = new PlaneRepository(_appContext));
+
+        public IRepository<PlaneType> PlaneTypeRepository => _planeTypeRepository ??
+                                                             (_planeTypeRepository = new PlaneTypeRepository(_appContext));
+
+        public IRepository<Steward> StewardRepository => _stewardRepository ??
+                                                         (_stewardRepository = new StewardRepository(_appContext));
+
+        public IRepository<Ticket> TicketRepository => _ticketRepository ??
+                                                       (_ticketRepository = new TicketRepository(_appContext));
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
