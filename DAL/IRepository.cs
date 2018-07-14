2f68e74f572e6c08ad7b4e07f40fd87baf608128
Gregory Golovko <golovko.greg@gmail.com>
2018-07-15 01:41:34 +0300

    Realized data access layer

---
+++ DAL/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+
+namespace DAL
+{
+    public interface IRepository<TEntity> where TEntity : BaseModel, new()
+    {
+        TEntity Create(TEntity item);
+
+        void Delete(Guid id);
+
+        TEntity Get(Guid id);
+
+        IEnumerable<TEntity> GetList();
+
+        void Update(TEntity item);
+
+        void Save();
+    }
+}
